f4bf0583eb45991cfc5ef5bbab419ad64ddd7f06
Marlon Velasquez <marlonjvelasquez@gmail.com>
2021-04-29 17:51:07 +0200

    dropDown.vue Create a new component incharge of the dropdown menu.

---
+++ src/components/dropDown.vue
@@ -0,0 +1,88 @@
+<template>
+  <button class="menu-item" @click="isOpen = !isOpen">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      id="Capa_1"
+      x="0px"
+      y="0px"
+      width="20px"
+      height="20px"
+      viewBox="0 0 960 560"
+      enable-background="new 0 0 960 560"
+      xml:space="preserve"
+    >
+      <path
+        d="M480,344.181L268.869,131.889c-15.756-15.859-41.3-15.859-57.054,0c-15.754,15.857-15.754,41.57,0,57.431l237.632,238.937   c8.395,8.451,19.562,12.254,30.553,11.698c10.993,0.556,22.159-3.247,30.555-11.698l237.631-238.937   c15.756-15.86,15.756-41.571,0-57.431s-41.299-15.859-57.051,0L480,344.181z"
+      />
+    </svg>
+    <transition name="fade" appear>
+      <div class="sub-menu" v-if="isOpen">
+        <a class="myAcount" href="">Mis Cuentas</a>
+        <div class="toolTip">
+          <p>{{ time.hour }}:{{ time.minutes }}:{{ time.seconds }}</p>
+        </div>
+        <div v-for="(item, i) in items" :key="i" class="menu-item">
+          <a class="itemLink" href="#">{{ item.title }}</a>
+        </div>
+      </div>
+    </transition>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "dropdown",
+  props: ["items", "time"],
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+};
+</script>
+
+<style>
+.sub-menu {
+  -webkit-box-shadow: 0px 1px 10px 1px rgba(150, 150, 150, 1);
+  -moz-box-shadow: 0px 1px 10px 1px rgba(150, 150, 150, 1);
+  box-shadow: 0px 1px 10px 1px rgba(150, 150, 150, 1);
+}
+
+.menu-item .sub-menu {
+  position: absolute;
+  background-color: rgb(255, 255, 255);
+  border-radius: 15px;
+  top: calc(100% + 18px);
+  transform: translateX(-100%);
+  width: 25rem;
+  text-align: center;
+}
+
+.fade-enter-active,
+.fade-leaver-active {
+  transition: 0.5s ease-in-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.toolTip {
+  display: none;
+}
+
+.myAcount:hover + .toolTip {
+  display: block;
+  position: absolute;
+  background-color: inherit;
+  box-shadow: inherit;
+  border-radius: 15px;
+  top: 5px;
+  width: 25rem;
+  padding: 0.5rem 0;
+  transform: translateX(-101%);
+}
+</style>
